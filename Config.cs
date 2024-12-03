a05bf709616cd5dc9b9cf7f295a8a8b490e63142
Sofia <sofakurerova@gmail.com>
2024-12-03 13:35:46 +0300

    Обновление

--- Config.cs
+++ Config.cs
@@ -8,9 +8,9 @@ namespace ServerApp
         public string PostgresHost { get; set; } = "localhost";
         public int PostgresPort { get; set; } = 5432;
         public string PostgresUser { get; set; } = "postgres";
-        public string PostgresPassword { get; set; } = "B62M5RTljJNiGtguqomf";
+        public string PostgresPassword { get; set; } = "12345678";
         public int ApiPort { get; set; } = 8080;
-        public string Database { get; set; } = "accounting";
+        public string Database { get; set; } = "acc";
         public string SchemaPath { get; set; } = "schema.json";
 
         public static string GetConfigPath(string[] args)
